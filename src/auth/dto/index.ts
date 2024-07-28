ba03a57c5f073c71462606916794550f5793b4c3
Soheil <soheilzarepoor79@gmail.com>
2024-07-28 17:42:39 +0330

    chore: add swagger

--- src/auth/dto/index.ts
+++ src/auth/dto/index.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
+import { createZodDto } from "@wahyubucil/nestjs-zod-openapi";
 
 export const loginSchema = z.object({
     email: z.string().email(),
     password: z.string(),
 });
-export type LoginDto = z.infer<typeof loginSchema>;
+
+// export type LoginDto = z.infer<typeof loginSchema>;
+
+export class LoginDto extends createZodDto(loginSchema) {}
+export class RegisterDto extends createZodDto(loginSchema) {}
